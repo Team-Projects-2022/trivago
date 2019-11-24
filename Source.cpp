250f85dd08fdc6fa0049e4cff761de82e11f2815
alaahassan2022 <57592600+alaahassan2022@users.noreply.github.com>
2019-11-25 00:49:37 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,191 +3,241 @@
 #include<string>
 using namespace std;
 void Setup(HotelList& h);
+
 int main()
 {
+
+
+		HotelList hotels;
+		Setup(hotels);
+		bool c = true;
+		while (c)
+		{
+			int admin;
+			cout << "Login :" << endl;
+			cout << endl;
+			cout << "1.Admin     2.User     3.Exit" << endl;
+			while (true)
+			{
+				cin >> admin;
+				if (admin > 3 || admin < 0)
+				{
+					cout << "Enter a valid number" << endl;
+				}
+				else
+				{
+					system("cls");
+					break;
+				}
+			}
+			switch (admin)
+			{
+			case 1:
+			{
+					  bool c1 = true;
+					  while (c1)
+					  {
+						  cout << "Logged in as <Admin>" << endl << endl;
+						  cout << "1.Add Hotel" << endl;
+						  cout << "2.Update Hotel" << endl;
+						  cout << "3.Delete Hotel" << endl;
+						  cout << "4.Display Hotel's info" << endl;
+						  cout << "5.Search & Filter Hotels" << endl;
+						  cout << "6.Exit" << endl;
+						  cout << "choose what you want to do : ";
+						  int x;
+						  while (true)
+						  {
+							  cin >> x;
+							  if (x > 6 || x < 1)
+							  {
+								  cout << "Enter a valid number : ";
+							  }
+							  else
+							  {
+								  system("cls");
+								  break;
+							  }
+						  }
+						  switch (x)
+						  {
+						  case 1:
+						  {
+									hotels.Add_hotel_Admin();
+									system("pause");
+									system("cls");
+									break;
+						  }
+						  case 2:
+						  {
+									hotels.Update_hotel();
+									system("pause");
+									system("cls");
+									break;
+						  }
+						  case 3:
+						  {
+									hotels.Delete_hotel();
+									system("pause");
+									system("cls");
+									break;
+						  }
+						  case 4:
+						  {
+									hotels.Display_hotels();
+									system("pause");
+									system("cls");
+									break;
+						  }
+						  case 5:
+						  {
+									int y;
+									cout << "Search by availability or Filter hotels (1/2) : ";
+									cin >> y;
+									if (y == 1)
+									{
+										hotels.searchhotel();
+										system("pause");
+										system("cls");
+									}
+									else
+									{
+										hotels.search();
+										system("pause");
+										system("cls");
+									}
+									break;
+						  }
+						  case 6:
+						  {
+									c1 = false;
+									break;
+						  }
+						  }
+
+					  }
+					  break;
+			}
+			case 2:
+			{
+					  bool c2 = true;
+					  while (c2)
+					  {
+						  cout << "Logged in as <User>" << endl << endl;
+						  cout << "1.Search for available hotels" << endl;
+						  cout << "2.Search for hotels with number of stars / rating" << endl;
+						  cout << "3.Search for hotels with a specific query" << endl;
+						  cout << "4.Exit" << endl;
+						  cout << "choose what you want to do : ";
+						  int x;
+						  while (true)
+						  {
+							  cin >> x;
+							  if (x > 6 || x < 1)
+							  {
+								  cout << "Enter a valid number : ";
+							  }
+							  else
+							  {
+								  system("cls");
+								  break;
+							  }
+						  }
+						  switch (x)
+						  {
+						  case 1:
+						  {
+									hotels.searchhotel();
+									system("pause");
+									system("cls");
+									break;
+						  }
+						  case 2:
+						  {
+									hotels.filter();
+									system("pause");
+									system("cls");
+									break;
+						  }
+						  case 3:
+						  {
+									hotels.search();
+									system("pause");
+									system("cls");
+									break;
+						  }
+						  case 4:
+						  {
+									c2 = false;
+									break;
+						  }
+						  }
+					  }
+					  break;
+			}
+			case 3:
+			{
+					  c = false;
+					  break;
+			}
+			}
+		}
+	
+
+	
+	return 0;
+
+	/*adminAdd();
+	void adminAdd(){
+	bool ans; int add;
 	HotelList hotels;
-	Setup(hotels);
-	bool c = true;
-	while (c)
-	{
-		int admin;
-		cout << "Login :" << endl;
-		cout << endl;
-		cout << "1.Admin     2.User     3.Exit" << endl;
-		while (true)
-		{
-			cin >> admin;
-			if (admin > 3 || admin < 0)
-			{
-				cout << "Enter a valid number" << endl;
-			}
-			else
-			{
-				system("cls");
-				break;
-			}
-		}
-		switch (admin)
-		{
-		case 1:
-		{
-			bool c1 = true;
-			while (c1)
-			{
-				cout << "Logged in as <Admin>" << endl << endl;
-				cout << "1.Add Hotel" << endl;
-				cout << "2.Update Hotel" << endl;
-				cout << "3.Delete Hotel" << endl;
-				cout << "4.Display Hotel's info" << endl;
-				cout << "5.Search & Filter Hotels" << endl;
-				cout << "6.Exit" << endl;
-				cout << "choose what you want to do : ";
-				int x;
-				while (true)
-				{
-					cin >> x;
-					if (x > 6 || x < 1)
-					{
-						cout << "Enter a valid number : ";
-					}
-					else
-					{
-						system("cls");
-						break;
-					}
-				}
-				switch (x)
-				{
-				case 1:
-				{
-					hotels.Add_hotel();
-					system("pause");
-					system("cls");
-					break;
-				}
-				case 2:
-				{
-					hotels.Update_hotel();
-					system("pause");
-					system("cls");
-					break;
-				}
-				case 3:
-				{
-					hotels.Delete_hotel();
-					system("pause");
-					system("cls");
-					break;
-				}
-				case 4:
-				{
-					hotels.Display_hotels();
-					system("pause");
-					system("cls");
-					break;
-				}
-				case 5:
-				{
-					int y;
-					cout << "Search by availability or Filter hotels (1/2) : ";
-					cin >> y;
-					if (y == 1)
-					{
-						hotels.searchhotel();
-						system("pause");
-						system("cls");
-					}
-					else
-					{
-						hotels.search();
-						system("pause");
-						system("cls");
-					}
-					break;
-				}
-				case 6:
-				{
-					c1 = false;
-					break;
-				}
-				}
-
-			}
-			break;
-		}
-		case 2:
-		{
-			bool c2 = true;
-			while (c2)
-			{
-				cout << "Logged in as <User>" << endl << endl;
-				cout << "1.Search for available hotels" << endl;
-				cout << "2.Search for hotels with number of stars / rating" << endl;
-				cout << "3.Search for hotels with a specific query" << endl;
-				cout << "4.Exit" << endl;
-				cout << "choose what you want to do : ";
-				int x;
-				while (true)
-				{
-					cin >> x;
-					if (x > 6 || x < 1)
-					{
-						cout << "Enter a valid number : ";
-					}
-					else
-					{
-						system("cls");
-						break;
-					}
-				}
-				switch (x)
-				{
-				case 1:
-				{
-					hotels.searchhotel();
-					system("pause");
-					system("cls");
-					break;
-				}
-				case 2:
-				{
-					hotels.filter();
-					system("pause");
-					system("cls");
-					break;
-				}
-				case 3:
-				{
-					hotels.search();
-					system("pause");
-					system("cls");
-					break;
-				}
-				case 4:
-				{
-					c2 = false;
-					break;
-				}
-				}
-			}
-			break;
-		}
-		case 3:
-		{
-			c = false;
-			break;
-		}
-		}
+	cout << "Click 1 to add hotel\n";
+	cin >> add;
+	if (add==1)
+	hotels.Add_hotel_Admin();
+	cout << "Again? 0/1";
+	cin >> ans;
+	if (ans == 1){
+	system("cls");
+	adminAdd();
 	}
-}
 
+	}
+	HotelList x;
+	Hotel h;
+	Room z;
+	h.ID = 1;
+	h.Name = "Hotel transylvania";
+	h.Number_Of_Stars = 4;
+	h.Country = "Egypt";
+	h.Location = "cairo";
+	h.Free_Meals = "breakfast-lunch";
+	h.Rate = 8.5;
+	h.Has_pool = true;
+	h.Has_Gym = false;
+	h.Available = true;
+	/////////////////////////////////// hotel
+	z.Has_TV = true;
+	z.Has_TV = true;
+	z.Reservation_Duration = 14;
+	z.reserved = false;
+	z.Room_Number = 1;
+	z.Singularity = false;
+	/////////////////////////////////// room
+	h.rooms.Add_Room(z);
+	x.Add_hotel(h);
+	x.Update_hotel();
+	*/
+}
 void Setup(HotelList& h)
 {
 	Hotel val;
-	
+
 	val.Available = true;
 	val.Country = "Egypt";
-	val.Free_Meals = "Breakfast";
+	val.Free_Meals[0] = true;
+	val.Free_Meals[1] = true;
+
 	val.Has_Gym = false;
 	val.Has_pool = true;
 	val.ID = 1000;
@@ -202,7 +252,8 @@ void Setup(HotelList& h)
 	//////////////////////////////////////
 	val.Available = true;
 	val.Country = "Egypt";
-	val.Free_Meals = "";
+	val.Free_Meals[0] = true;
+	val.Free_Meals[1] = true;	
 	val.Has_Gym = false;
 	val.Has_pool = false;
 	val.ID = 1001;
@@ -217,7 +268,8 @@ void Setup(HotelList& h)
 	////////////////////////////////////
 	val.Available = true;
 	val.Country = "France";
-	val.Free_Meals = "Breakfast/Lunch/Dinner";
+	val.Free_Meals[0] = true;
+	val.Free_Meals[1] = true;
 	val.Has_Gym = true;
 	val.Has_pool = true;
 	val.ID = 1002;
@@ -232,7 +284,8 @@ void Setup(HotelList& h)
 	////////////////////////////////////
 	val.Available = false;
 	val.Country = "USA";
-	val.Free_Meals = "Lunch/Dinner";
+	val.Free_Meals[0] = true;
+	val.Free_Meals[1] = true;	
 	val.Has_Gym = false;
 	val.Has_pool = true;
 	val.ID = 1003;
@@ -247,7 +300,9 @@ void Setup(HotelList& h)
 	/////////////////////////////////////
 	val.Available = true;
 	val.Country = "USA";
-	val.Free_Meals = "Breakfast/Lunch/Dinner";
+	val.Free_Meals[0] = true;
+	val.Free_Meals[1] = false;
+
 	val.Has_Gym = true;
 	val.Has_pool = true;
 	val.ID = 1004;
@@ -262,7 +317,8 @@ void Setup(HotelList& h)
 	/////////////////////////////////////
 	val.Available = true;
 	val.Country = "India";
-	val.Free_Meals = "";
+	val.Free_Meals[0] = true;
+	val.Free_Meals[1] = true;	
 	val.Has_Gym = true;
 	val.Has_pool = true;
 	val.ID = 1005;
@@ -276,6 +332,5 @@ void Setup(HotelList& h)
 	newnode5->next = newnode6;
 
 	h.tail = newnode6;
-	
-}
 
+}
