8e427711556e1a0d12f9026313e31f75a349803b
alaahassan2022 <57592600+alaahassan2022@users.noreply.github.com>
2019-11-25 00:52:50 +0200

    Update Hotel.h

--- Hotel.h
+++ Hotel.h
@@ -31,7 +31,7 @@ public:
 	int Number_Of_Rooms;
 public:
 	RoomList();
-	void Add_Room();
+	void Add_Room_Admin();
 	void Update_Room();
 	void searchroom(int no);
 };
@@ -44,7 +44,7 @@ struct Hotel
 	string Name;
 	string Country;
 	string Location;
-	string Free_Meals;
+	bool Free_Meals[2];
 	string comments;
 	int Number_Of_Stars;
 	int Number_Of_Rooms;
@@ -68,12 +68,14 @@ public:
 
 class HotelList
 {
-	HotelNode* head;
+
+public:		
 	HotelNode* tail;
+
 	int Number_Of_Hotels;
-public:
+	HotelNode* head;
 	HotelList();
-	void Add_hotel();
+	void Add_hotel_Admin();
 	void Update_hotel();
 	void Delete_hotel();
 	void Display_hotels();
